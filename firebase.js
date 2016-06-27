9217da7fc4c4a6f2e803429287480e88883e63b7
LucCote <LucCote@stmarksschool.org>
2016-06-26 22:12:46 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+var config = {
+	    apiKey: "AIzaSyDtO-Gz6MDt5BvbmdfqSOtpMwYIuBNLoIA",
+	    authDomain: "diceroller-f0a1a.firebaseapp.com",
+	    databaseURL: "https://diceroller-f0a1a.firebaseio.com",
+	    storageBucket: "diceroller-f0a1a.appspot.com",
+	  };
+	  firebase.initializeApp(config);
