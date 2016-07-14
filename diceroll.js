10507cf2ec89265fa92f1b0cbb52bfe1988b9e2a
LucCote <LucCote@stmarksschool.org>
2016-07-13 23:20:13 -0400

    Update diceroll.js

--- diceroll.js
+++ diceroll.js
@@ -50,6 +50,6 @@ $(document).ready(function(){
 		}else{
 			alert("you can only roll up to 500 dice");
 		}
-		document.getElementById("#inputs").reset();
+		document.getElementById("inputs").reset();
 	});
 });
